b5c39fa933fbf6aff93d0d64c54873601037dc66
{Thorkild556} <{thorkild234@gmail.com}>
2023-02-28 23:00:46 +0100

    very first edition.

--- Health_app_folder/healthapp.js
+++ Health_app_folder/healthapp.js
@@ -1,1 +1,6 @@
+const button = document.getElementById("button");
 
+button.addEventListener("click", function() {
+    const bloodPressure = prompt("Please enter your blood pressure:");
+    console.log(bloodPressure);
+  });
