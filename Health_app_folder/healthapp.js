5ce43b08f3b4f4a1d88b46915da45d8be1bb42ec
{Thorkild556} <{thorkild234@gmail.com}>
2023-03-01 13:40:43 +0100

    Added plots, without integration to the app.

--- Health_app_folder/healthapp.js
+++ Health_app_folder/healthapp.js
@@ -1,6 +1,16 @@
-const button = document.getElementById("button");
+const button_report = document.getElementById("button");
 
-button.addEventListener("click", function() {
+button_report.addEventListener("click", function() {
     const bloodPressure = prompt("Please enter your blood pressure:");
     console.log(bloodPressure);
-  });
+
+    const currentDate = new Date();
+    console.log(currentDate);
+
+    const data = {
+      bloodPressure: bloodPressure,
+      currentDate: currentDate,
+    };
+
+});
+
--- Health_app_folder/plots.py
+++ Health_app_folder/plots.py
@@ -1,3 +1,18 @@
-#this is the plotting script.
+#This is the plotting script
 
-import matplotlib as mpl
+import matplotlib.pyplot as plt
+import pandas as pd
+
+date_time = ["2023-01-13","2023-01-29","2023-02-15","2023-02-26","2023-03-01"]
+blood_pressure = [120, 110, 100, 120, 90]
+
+date_time = pd.to_datetime(date_time)
+
+df = pd.DataFrame(blood_pressure, date_time)
+
+plt.plot(df)
+plt.xlabel("Date Time")
+plt.ylabel("Blood Pressure")
+plt.savefig('Health_app_folder/plots/plot.png')
+
+
