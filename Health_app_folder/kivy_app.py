c8b203332f9992314e38f7df6c7a2805bcbe3040
{Thorkild556} <{thorkild234@gmail.com}>
2023-03-03 19:27:03 +0100

    kivyapp_0.2

--- Health_app_folder/kivy_app.py
+++ Health_app_folder/kivy_app.py
@@ -3,12 +3,27 @@
 import kivy
 import matplotlib.pyplot as plt
 import pandas as pd
+from datetime import date
+from datetime import datetime
 
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+
+
+bloodpresure_list = []
+date_list = []
+test_dates5 = ['03/12/22', '01/01/23', '01/05/23', '15/06/23', '1/07/23']
+
+test_dates5 = pd.to_datetime(test_dates5, format="%d/%m/%y")
+
+
+
+today = date.today()
+today = today.strftime("%d/%m/%y")
+
 
 class ChildApp(GridLayout): 
     def __init__(self, **kwargs):
@@ -23,8 +38,27 @@ class ChildApp(GridLayout):
         self.press.bind(on_press = self.report)
         self.add_widget(self.press)
 
+        self.press = Button(text= "See Health data")
+        self.press.bind(on_press = self.see_health)
+        self.add_widget(self.press)
+
+
     def report(self, instance):
         print("Your blood presure is "+self.s_bloodpressure.text)
+        blodd_pressure = int(self.s_bloodpressure.text)
+        bloodpresure_list.append(blodd_pressure)
+        date_list.append(today)
+        print(bloodpresure_list, test_dates5)
+        
+        if blodd_pressure > 120:
+            print("Go to your doctor")
+
+    def see_health(self, instance):
+        df = pd.DataFrame( bloodpresure_list, test_dates5)
+        plt.plot(df)
+        plt.show()
+
+    
 
 class ParentApp(App):
     def build(self):
