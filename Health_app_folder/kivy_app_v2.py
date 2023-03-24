d4f7d9f35379f020599c40aa5d09a75b7f9ad1af
esthergade <112488669+esthergade@users.noreply.github.com>
2023-03-24 14:18:01 +0100

    With pop-upbox

--- Health_app_folder/kivy_app_v2.py
+++ Health_app_folder/kivy_app_v2.py
@@ -12,6 +12,11 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.uix.behaviors import ButtonBehavior
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+
+
 
 #defining lists that will be used later
 bloodpresure_sys_list = []
@@ -19,6 +24,12 @@ bloodpresure_dia_list = []
 heartrate_list = []
 date_list = []
 test_dates5 = ['03/12/22', '01/01/23', '01/05/23', '15/06/23', '1/07/23'] #this is a test list
+
+
+try:
+    df = pd.read_csv('./csv_data/health_data.csv')
+except FileNotFoundError:
+    df = pd.DataFrame({'systolic': bloodpresure_sys_list, 'diastolic': bloodpresure_dia_list, 'Heartrate': heartrate_list, 'date': date_list})
 
 test_dates5 = pd.to_datetime(test_dates5, format="%d/%m/%y") #converting to actual dates instead of text
 
@@ -63,18 +74,22 @@ class ChildApp(GridLayout):
         blood_pressure_dia = int(self.s_bloodpressure_dia.text)
         heartrate = int(self.s_heartrate.text)
         #appending to the lists so we can make a plot
+
         bloodpresure_sys_list.append(blood_pressure_sys)
         bloodpresure_dia_list.append(blood_pressure_dia)
         heartrate_list.append(heartrate)
         date_list.append(today) #i defined "today" ealier in the code
-        #making a datafram so we can save a csv-file.
-        #creating a dictionary from our data
-        dict = {'systolic': bloodpresure_sys_list, 'diastolic': bloodpresure_dia_list, 'Heartrate': heartrate, 'date': date_list}
-        df = pd.DataFrame(dict)
+
+
+        dict2 = {'systolic': bloodpresure_sys_list, 'diastolic': bloodpresure_dia_list, 'Heartrate': heartrate_list, 'date': date_list}
+        df2 = pd.DataFrame(dict2)
+        df3 = pd.concat([df, df2])
+        print(df3)
+
 
         #printing to see if it works.
         print(bloodpresure_sys_list, bloodpresure_dia_list, test_dates5)
-        
+
         # creating conditionals for pop ups
         if blood_pressure_sys <= 120 and blood_pressure_dia <= 80:
             print("optimal blood pressure")
@@ -87,14 +102,39 @@ class ChildApp(GridLayout):
         elif blood_pressure_sys >= 180 or blood_pressure_dia >= 120:
             print("blood pressure in hypertensive emergency - seek medical care immediately")
 
-        #saving a csv
-        df.to_csv('./csv_data/health_data.csv')
-# if you click on the see health data button, then it will show a plot of the reported health data.
+ # if you click on the see health data button, then it will show a plot of the reported health data.
     def see_health(self, instance):
-        plt.plot(test_dates5, bloodpresure_sys_list, color ='r', label = "systolic")
-        plt.plot(test_dates5, bloodpresure_dia_list, color ='g', label = "diastolic")
+        df3 = pd.read_csv('./csv_data/health_data.csv')
+        plt.plot(df3['date'], df3['systolic'], color ='r', label = "systolic")
+        plt.plot(df3['date'], df3['diastolic'], color ='g', label = "diastolic")
         plt.legend()
         plt.show()
+
+        #saving a csv
+        df3.to_csv('./csv_data/health_data.csv')
+
+
+class MyBoxLayout(BoxLayout):
+    def on_text(self, instance, value):
+        try:
+            blood_pressure_sys, blood_pressure_dia = map(int, value.split())
+            if  blood_pressure_sys <= 120 and blood_pressure_dia <= 80:
+                print("optimal blood pressure")
+            elif 120 < blood_pressure_sys <= 129 and blood_pressure_dia <= 80:
+                print("elevated blood pressure")
+            # Add more conditions here
+        except ValueError:
+            pass
+    
+
+class MyApp(App):
+    def build(self):
+        return MyBoxLayout()
+
+
+if __name__ == '__main__':
+    MyApp().run()
+
 
 
 
